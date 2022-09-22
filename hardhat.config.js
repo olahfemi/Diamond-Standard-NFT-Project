0156c8aaad86a0bcb25c63394a9f8967348ab312
Femi Olah <olahfemi@gmail.com>
2022-09-22 14:58:53 +0100

    Diamond Standard NFT project

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+
+/* global ethers task */
+require('@nomiclabs/hardhat-waffle')
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.6',
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_GOERLI_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: "VUZ7GFPUYG8UGNZXS635722912XFCDBI67"
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
